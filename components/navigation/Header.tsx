06c21486462b1efb60ade164f38709007b44d7eb
Nwokedi Philip <philippos764@gmail.com>
2024-10-16 13:31:09 +0100

    chore: remove login link

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -10,17 +10,14 @@ const Header: React.FC<IHeader> = ({ className, children, ...headerProps }) => {
         aria-label="Global"
       >
         <Link href="/" className="flex flex-row items-center gap-x-1 lg:flex-1">
-          {/* <span className="relative w-5 h-5 md:w-6 md:h-6 lg:w-24 lg:h-24 -m- p-0">
-            <span className="sr-only">MIVA</span>
-          </span> */}
           <span className="font-satoshi-variable font-medium md:font-bold text-sm md:text-lg text-gray-900">
             MIVA UNIVERSITY
           </span>
         </Link>
-        <div className="hidden lg:flex lg:gap-x-scale-desktop-39 items-center">
-          <Link href="/login" className="">
+        <div className="flex lg:gap-x-scale-desktop-39 items-center">
+          {/* <Link href="/login" className="">
             Login
-          </Link>
+          </Link> */}
         </div>
       </nav>
     </header>
